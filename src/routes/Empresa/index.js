a4f43b56e1e24742c217fb2fe5e1ff067b5ea8b5
Mauricio Campo <mcampo26@gmail.com>
2024-07-11 13:45:06 -0300

    agrefgo detalles al escaneo

--- src/routes/Empresa/index.js
+++ src/routes/Empresa/index.js
@@ -1,18 +1,20 @@
 import express from "express";
-import { 
-  createEmpresa, 
-  getEmpresas, 
-  updateEmpresa, 
-  deleteEmpresaById, 
-  getAllEmpresas 
-} from '../../controllers/empresaControllers/index.js';
+import {
+  createEmpresa,
+  getEmpresas,
+  updateEmpresa,
+  deleteEmpresaById,
+  getAllEmpresas,
+  getEmpresaById
+} from "../../controllers/empresaControllers/index.js";
 
 const router = express.Router();
 
-router.post('/create', createEmpresa);
-router.get('/get', getEmpresas);
-router.get('/all', getAllEmpresas); // Nueva ruta para obtener todas las empresas sin paginación
-router.put('/update/:id', updateEmpresa);
-router.delete('/delete/:id', deleteEmpresaById);
+router.post("/create", createEmpresa);
+router.get("/get", getEmpresas);
+router.get("/all", getAllEmpresas);
+router.get("/:id", getEmpresaById);
+router.put("/update/:id", updateEmpresa);
+router.delete("/delete/:id", deleteEmpresaById);
 
 export default router;
