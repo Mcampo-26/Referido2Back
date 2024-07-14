f9b3f886317302cb94a05e8981c6a0fda2c1fba5
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-07-14 10:35:16 -0300

    Update Index.js

--- src/routes/Servicio/Index.js
+++ src/routes/Servicio/Index.js
@@ -5,13 +5,15 @@ import {
   updateServicio,
   deleteServicioById,
   getAllServicios,
+  getServiciosByEmpresaId, // Asegúrate de importar esta función
 } from "../../controllers/servicioControllers/index.js";
 
 const router = express.Router();
 
 router.post("/create", createServicio);
 router.get("/get", getServicios);
-router.get("/all", getAllServicios); // Nueva ruta para obtener todos los servicios sin paginación
+router.get("/all", getAllServicios);
+router.get("/byEmpresa/:empresaId", getServiciosByEmpresaId); // Agrega esta línea
 router.put("/update/:id", updateServicio);
 router.delete("/delete/:id", deleteServicioById);
 
