ef95da17675b33d48cb979e218510a8a757099bc
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-07-17 14:40:07 -0300

    Update Servicio.js

--- src/models/Servicio.js
+++ src/models/Servicio.js
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 // Esquema para el modelo de Servicio
 const ServicioSchema = new Schema({
   name: { type: String, required: [true, "Nombre es requerido"] },
-  // Puedes agregar otros campos aquí según las necesidades de tu aplicación
+  empresaId: { type: Schema.Types.ObjectId, ref: 'Empresa', required: true }
 });
 
 export default model("Servicio", ServicioSchema);
