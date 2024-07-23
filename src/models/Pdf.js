80247b4c3fcaabfee684e16f0b0a9a69707b3dff
Mauricio Campo <mcampo26@gmail.com>
2024-07-22 23:31:55 -0300

    agredo detalles en pdf

--- src/models/Pdf.js
+++ src/models/Pdf.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // models/Pdf.js
 import mongoose from "mongoose";
 
@@ -18,24 +17,3 @@ const PdfSchema = new mongoose.Schema({
 });
 
 export default mongoose.model("Pdf", PdfSchema);
-=======
-// models/Pdf.js
-import mongoose from "mongoose";
-
-const PdfSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-export default mongoose.model("Pdf", PdfSchema);
->>>>>>> c2e165e690364d3930de25f96960e6d9bb5ede11
--- src/routes/Pdf/index.js
+++ src/routes/Pdf/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // routes/pdfRoutes.js
 import express from "express";
 import {
@@ -20,26 +19,3 @@ router.put("/update/:id", updatePdf);
 router.delete("/delete/:id", deletePdf);
 
 export default router;
-=======
-// routes/pdfRoutes.js
-import express from "express";
-import {
-  createPdf,
-  getPdfs,
-  getPdfById,
-  downloadPdf,
-  updatePdf,
-  deletePdf,
-} from "../../controllers/pdfControllers/index.js";
-
-const router = express.Router();
-
-router.post("/create", createPdf);
-router.get("/get", getPdfs);
-router.get("/:id", getPdfById);
-router.get("/download/:id", downloadPdf); // Ruta para descargar el PDF
-router.put("/update/:id", updatePdf);
-router.delete("/delete/:id", deletePdf);
-
-export default router;
->>>>>>> c2e165e690364d3930de25f96960e6d9bb5ede11
